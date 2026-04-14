03625863ac3014a99f817af0d324869376f2d321
CarolineTikhomirova <tih.karolina@yandex.ru>
2026-04-14 21:32:27 +0400

    feat: implement image list table view and custom cell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Caroline on 09.04.2026.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+}

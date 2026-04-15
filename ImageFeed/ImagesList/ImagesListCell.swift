589526777abff0b280bfdd1164c297577010bfea
CarolineTikhomirova <tih.karolina@yandex.ru>
2026-04-15 14:45:47 +0400

    style: apply review fixes for ImagesListCell layout

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    static let reuseIdentifier = "ImagesListCell"
     
     @IBOutlet var cellImage: UIImageView!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
+    @IBOutlet var gradientView: UIView!
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    private let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupGradient()
+        gradientView.layer.cornerRadius = 16
+        gradientView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        gradientView.clipsToBounds = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradient.frame = gradientView.bounds
+    }
+    
+    private func setupGradient() {
+        gradient.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.5).cgColor
+        ]
+        gradientView.layer.insertSublayer(gradient, at: 0)
+    }
 }

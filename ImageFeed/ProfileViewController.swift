9f75f9905738b20ab685c3d1dc34ae15c9eeeff1
CarolineTikhomirova <tih.karolina@yandex.ru>
2026-04-17 15:42:27 +0400

    feat: implement ProfileViewController UI and outlets

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Caroline on 16.04.2026.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+    
+}
